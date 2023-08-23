cbf9a22305a52077e06a2ecc5c45559a25cd5d13
Victoria Denisse Reynoso <victoria.d.reynoso.88@gmail.com>
2023-08-23 03:05:39 -0300

    Creación clase PacienteVO.

--- agendaTurnos/src/main/java/com/agendaTurnos/vo/PacienteVO.java
+++ agendaTurnos/src/main/java/com/agendaTurnos/vo/PacienteVO.java
@@ -2,4 +2,57 @@ package com.agendaTurnos.vo;
 
 public class PacienteVO {
 
+	private int dni;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String telefono;
+	
+	public PacienteVO(int dni, String nombre, String apellido, String email, String telefono) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.telefono = telefono;
+	}
+	
+	public int getDni() {
+		return dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	@Override
+	public String toString() {
+		return "PacienteVO [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email
+				+ ", telefono=" + telefono + "]";
+	}
+	
+	
+	
 }
